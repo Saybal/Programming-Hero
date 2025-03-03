c10b5863aa67d30c15eebb5914fb76aea8c7c2ea
Saybal <roysaybal@gmail.com>
2025-03-03 22:04:44 +0600

    Assignment 05

---
+++ Assignment 5/Scripts/blogs.js
@@ -0,0 +1,12 @@
+// console.log("AMi je jolsha kori")
+
+// Main page e jaoar event
+document.querySelector("#back").addEventListener("click",
+    function (event)
+    {
+        event.preventDefault();
+        window.location.href = "index.html";
+        
+    })
+
+    
